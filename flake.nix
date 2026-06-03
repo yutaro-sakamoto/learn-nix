291b6c57cd6ee05e48ec7a87cc81dfa971ef7320
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-06-04 02:01:37 +0900

    wip: wip

--- flake.nix
+++ flake.nix
@@ -3,6 +3,16 @@
         nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     };
 
-    outputs = _: {
-    };
+    outputs =
+        { nixpkgs, ... }:
+        let
+            system = "x86_64-linux";
+            pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+            packages.${system} = {
+                hello = pkgs.hello;
+            };
+        };
+
 }
