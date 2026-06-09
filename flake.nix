ebbfa45a9b7792cb314e37bd307181e8b8ea964b
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-06-09 22:47:42 +0900

    wip: wip

--- flake.nix
+++ flake.nix
@@ -1,24 +1,29 @@
 {
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-        flake-utils.url = "github:numtide/flake-utils";
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nur.url = "github:nix-community/NUR";
+  };
 
-    outputs =
-        { nixpkgs, flake-utils, ... }:
-        flake-utils.lib.eachDefaultSystem (
-            system:
-            let
-                pkgs = nixpkgs.legacyPackages.${system};
-            in
-            {
-                packages.default = pkgs.mkShell {
-                    packages = with pkgs; [
-                        nodejs_20
-                        corepack
-                    ];
-                };
-            }
-        );
-
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      nur,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        # `nur.overlays.<overlayの名前>`ではなく`nur.overlay`
+        # `outputs.overlay`は現在非推奨だが恐らく後方互換性のために残されている
+        overlays = [ nur.overlay ];
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      {
+        packages = {
+          default = pkgs.nur.repos.mic92.hello-nur;
+        };
+      }
+    );
 }
