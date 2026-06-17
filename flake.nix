3acb926f0ac41ab4dba1ebfefd6bf7263d25e8af
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-06-17 21:58:48 +0900

    wip: wip

--- flake.nix
+++ flake.nix
@@ -1,33 +1,15 @@
 {
+  description = "A very basic flake";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    gnu-hello-src = {
-      url = "https://ftp.gnu.org/gnu/hello/hello-2.12.tar.gz";
-      flake = false;
-    };
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
   outputs =
     {
-      nixpkgs,
-      flake-utils,
-      gnu-hello-src,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages = {
-          hello = pkgs.stdenv.mkDerivation {
-            pname = "hello";
-            version = "2.12";
-            src = gnu-hello-src;
-          };
-        };
-      }
-    );
+      self, nixpkgs
+    }: {
+      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+    };
 }
