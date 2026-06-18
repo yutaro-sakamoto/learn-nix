6e280dd30d77cdd6583df840a8ae419151fd413a
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-06-18 22:03:39 +0900

    wip: wip

--- flake.nix
+++ flake.nix
@@ -1,15 +1,25 @@
 {
-  description = "A very basic flake";
+  description = "hello package written in Rust";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
     {
-      self, nixpkgs
-    }: {
-      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-      packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-    };
+      nixpkgs, flake-utils, ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          hello = pkgs.hello;
+          default = pkgs.hello;
+        };
+      }
+    );
 }
