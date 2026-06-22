be0a29d8ba14d6b0913788f181d60540bfb90c01
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-06-22 21:17:30 +0900

    wip: wip

--- nix/rust-with-nix.nix
+++ nix/rust-with-nix.nix
@@ -1,7 +1,14 @@
-{ rustPlatform }:
+{ makeRustPlatform, rust-bin }:
+let
+    toolchain = rust-bin.stable.latest.default;
+    rustPlatform = makeRustPlatform {
+        cargo = toolchain;
+        rustc = toolchain;
+    };
+in
 rustPlatform.buildRustPackage {
     pname = "rust-with-nix";
-    version "0.1.0";
+    version = "0.1.0";
     src = ../.;
     cargoLock.lockFile = ../Cargo.lock;
 }
