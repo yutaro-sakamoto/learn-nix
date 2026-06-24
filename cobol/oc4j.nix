89e83263cf63eb7518542c50d9685727f3566386
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2026-06-24 22:06:20 +0900

    wip: wip

--- cobol/oc4j.nix
+++ cobol/oc4j.nix
@@ -1,4 +1,4 @@
- { stdenv, fetchFromGitHub }:
+ { stdenv, fetchFromGitHub, makeWrapper, jdk }:
 stdenv.mkDerivation {
   pname = "o9pensourcecobol4j";
   version = "1.1.20";
@@ -8,11 +8,18 @@ stdenv.mkDerivation {
     rev = "a12dd685c58a562c12a09c943c0ac2a00f24d4cc";
     hash = "sha256-hsyEUDQ0LKezX/i9o80CPcL+8QWJJzOW7HsQORyRtv0=";
   };
+  nativeBuildInputs = [ makeWrapper jdk ];
+  buildInputs = [ jdk ];
   buildPhase = ''
     ./configure --prefix=$out
     make
   '';
   installPhase = ''
     make install
+  '';
+  postFixup = ''
+    for bin in $out/bin/*; do
+      wrapProgram "$bin" --prefix PATH : ${jdk}/bin
+    done
   '';
 }
